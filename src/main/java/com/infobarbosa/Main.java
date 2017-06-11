cd70fb33c9022a3f25985523a22b92dc786bd14d
infobarbosa <infobarbosa@yahoo.com.br>
2017-06-11 12:33:14 -0300

    obtencao dos links de uma pagina web passada como parametro com put no console e num kafka. precisa ser refatorado separando em elementos de microservicos.

--- src/main/java/com/infobarbosa/Main.java
+++ src/main/java/com/infobarbosa/Main.java
@@ -1,7 +1,11 @@
 package com.infobarbosa;
 
 import java.io.IOException;
+import java.util.Properties;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -36,6 +40,7 @@ public class Main{
 			  String linkHref = link.attr("href");
 			  String linkText = link.text();
 			  System.out.println( linkHref + " ------> "  + linkText);
+			  persistOnKafka(linkHref);
 			}
 		}catch(IOException e){
 			logger.error( e.toString() );
@@ -43,6 +48,24 @@ public class Main{
 			logger.error( e.toString() );
 			
 		}
+	}
+	
+	private void persistOnKafka(String url){
+		Properties props = new Properties();
+		 props.put("bootstrap.servers", "localhost:9092");
+		 props.put("acks", "all");
+		 props.put("retries", 0);
+		 props.put("batch.size", 16384);
+		 props.put("linger.ms", 1);
+		 props.put("buffer.memory", 33554432);
+		 props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		 props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-  }
+		 Producer<String, String> producer = new KafkaProducer<>(props);
+		 
+		 producer.send(new ProducerRecord<String, String>("teste1", this.url, url));
+
+		 producer.close();
+
+	}
 }

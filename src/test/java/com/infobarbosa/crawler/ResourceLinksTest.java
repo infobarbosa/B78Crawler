e91764cc02fe39b1973c9802e208c322e0eaf4db
Barbosa <infobarbosa@yahoo.com.br>
2018-01-14 00:16:45 -0200

    inclusao de testes unitarios de ResourceLink

--- src/test/java/com/infobarbosa/crawler/ResourceLinksTest.java
+++ src/test/java/com/infobarbosa/crawler/ResourceLinksTest.java
@@ -1,4 +1,51 @@
 package com.infobarbosa.crawler;
 
-public class ResourceLinksTest {
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ResourceLinksTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ResourceLinksTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ResourceLinksTest.class );
+    }
+
+    /**
+     * Teste da classe principal
+     */
+    public void testObtemLinksDeUmDocumento()
+    {
+        String url = "https://www.americanas.com.br/produto/132429722";
+        try {
+            Document doc = Jsoup.connect(url).get();
+            ResourceLinks resourceLinks = new ResourceLinks();
+            ArrayList<String> links = resourceLinks.list(doc);
+
+            if(links.size() > 0){
+                assertTrue(true);
+            }else{
+                fail("nenhum link encontrado.");
+            }
+        }catch(IOException ioe){
+            fail("falhou. ".concat(ioe.getMessage()));
+        }
+    }
 }

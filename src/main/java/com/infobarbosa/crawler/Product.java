a9e27b75993c33ec6db067672e5c2b5963a4af1a
Barbosa <infobarbosa@yahoo.com.br>
2018-01-14 00:03:04 -0200

    otimizacao da identificacao do produto considerando a webpage visitada

--- src/main/java/com/infobarbosa/crawler/Product.java
+++ src/main/java/com/infobarbosa/crawler/Product.java
@@ -4,9 +4,18 @@ import java.util.Objects;
 
 public class Product {
 
+    private String url; //a identificacao do produto depende da web page visitada
     private String id;
     private String description;
     private String price;
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    public String getUrl(){
+        return this.url;
+    }
 
     public void setId(String id) {
         this.id = id;
@@ -35,7 +44,8 @@ public class Product {
     @Override
     public String toString() {
         return "Product{" +
-                "id='" + id + '\'' +
+                "url='" + url + '\'' +
+                ", id='" + id + '\'' +
                 ", description='" + description + '\'' +
                 ", price='" + price + '\'' +
                 '}';
@@ -46,12 +56,13 @@ public class Product {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
-        return Objects.equals(id, product.id);
+        return Objects.equals(getUrl(), product.getUrl()) &&
+                Objects.equals(getId(), product.getId());
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(id);
+        return Objects.hash(getUrl(), getId());
     }
 }
